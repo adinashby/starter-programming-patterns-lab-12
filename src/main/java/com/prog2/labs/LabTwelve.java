b3c512c0ea1af4f3bc551487b00a44aeb1dff3ff
Adin Ashby <adin.ashby@gmail.com>
2022-10-14 01:08:46 -0400

    first commit

---
+++ src/main/java/com/prog2/labs/LabTwelve.java
@@ -0,0 +1,29 @@
+package com.prog2.labs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author adinashby
+ *
+ */
+
+public class LabTwelve {
+
+	/**
+	 * Write your test code below in the main (optional).
+	 *
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * Please refer to the README file for question(s) description
+	 */
+
+	public static int numberOfBoomerangs(int[][] points) {
+		return 0;
+	}
+
+}
